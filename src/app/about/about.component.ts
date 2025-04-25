4cd6f1db4b7ab816a0575d3b3ae3a536c0214904
mohamdii <122834211+mohamdii@users.noreply.github.com>
2025-04-25 21:21:33 +0300

    mh

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  experiences = signal([]);
+  keys: string[] = [];
+  httpClient = inject(HttpClient);
 
+  ngOnInit(): void {
+    this.httpClient.get<[]>('https://localhost:7269/Experience').subscribe({
+      next: (response) => {
+        console.log('Experience data:', response);
+        this.experiences.set(response);
+        if (response.length > 0) {
+          this.keys = Object.keys(response);
+
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching experience data:', error);
+      },
+    });
+  }
 }
