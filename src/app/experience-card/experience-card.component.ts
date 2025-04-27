bb833516c2b59735f9e494b3ae093ed7ccaf6e8f
mohamdii <122834211+mohamdii@users.noreply.github.com>
2025-04-27 14:21:42 +0300

    Added exp cards

--- src/app/experience-card/experience-card.component.ts
+++ src/app/experience-card/experience-card.component.ts
@@ -1,28 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import { IExperience } from '../Interfaces/IExperience';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-experience-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './experience-card.component.html',
   styleUrl: './experience-card.component.css',
 })
-export class ExperienceCardComponent implements OnInit {
-  localStorage = window.localStorage;
-  position = signal('Software Engineer');
-  companyName = signal('');
-  startDate = signal('');
-  endDate = signal('');
-  localStorageUsername = this.localStorage.getItem('username') || '';
-  httpClient = inject(HttpClient);
-
-  ngOnInit(): void {
-    this.httpClient
-      .get<IExperience>(`https://localhost:7269/Experience/${this.localStorageUsername}`)
-      .subscribe((data) => {
-        this.position.set(data.jobTitle);
-        console.log(data);
-      });
-  }
+export class ExperienceCardComponent {
+  @Input({ required: true }) experience!: IExperience;
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { ExperienceCardComponent } from "../experience-card/experience-card.component";
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ExperienceCardComponent } from '../experience-card/experience-card.component';
+import { HttpClient } from '@angular/common/http';
+import { IExperience } from '../Interfaces/IExperience';
 
 @Component({
   selector: 'app-hero',
   imports: [ExperienceCardComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  localStorage = window.localStorage;
+  experiences: IExperience[] = [];
+  localStorageUsername = this.localStorage.getItem('username') || '';
+  httpClient = inject(HttpClient);
 
+  ngOnInit(): void {
+    this.httpClient
+      .get<IExperience[]>('https://localhost:7269/Experience')
+      .subscribe((data) => {
+        this.experiences = data;
+        console.log(data);
+      });
+  }
 }
